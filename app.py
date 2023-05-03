c9913f93af946527cea66e7868bb780350a6f8cd
HanakoTetsuya <72732277+HanakoTetsuya@users.noreply.github.com>
2023-05-03 18:55:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,46 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+import re
+import string
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+from scikitplot.metrics import plot_confusion_matrix, plot_roc
+import streamlit as st
+
+data = pd.read_csv('dataset.csv')
+data=data.dropna(how='any')
+
+X = data["text"]
+y = data['label'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42, stratify = y)
+
+tfidf = TfidfVectorizer(max_features= 2500, min_df= 2)
+X_train = tfidf.fit_transform(X_train).toarray()
+X_test = tfidf.transform(X_test).toarray()
+
+def train_model(model):
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_prob = model.predict_proba(X_test)
+
+rf = RandomForestClassifier(n_estimators= 300)
+train_model(rf)
+
 
